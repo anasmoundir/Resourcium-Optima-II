12d3424866fc099f908e82afc23f8103e43e94ef
anasmoundir <anasmoundir1996@gmail.com>
2023-10-26 15:00:57 +0100

    DeparetementDao

--- src/main/java/com/example/optima/Dao/DeparetementDao.java
+++ src/main/java/com/example/optima/Dao/DeparetementDao.java
@@ -1,5 +1,26 @@
 package com.example.optima.Dao;
 
+import com.example.optima.Entities.DepartmentEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
 public class DeparetementDao {
+    private EntityManagerFactory entityManagerFactory;
+    public DeparetementDao(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+    public List<DepartmentEntity> getAllDepartments() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        try {
+            String jpql = "SELECT d FROM DepartmentEntity d";
+            TypedQuery<DepartmentEntity> query = em.createQuery(jpql, DepartmentEntity.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
 
 }
